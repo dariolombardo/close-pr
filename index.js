acc2377b8780e0684800772e4f37771cb17f0f1f
Dario Lombardo <lomato@gmail.com>
2020-04-04 23:43:00 +0200

    Revert "add async"
    This reverts commit 483fb8c591ad8f248785849564068080d14ba7a9.

--- index.js
+++ index.js
@@ -1,29 +1,27 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-async () => {
-  try {
-    const context = github.context;
-    const comment = core.getInput('comment');
-    const token = process.env["GITHUB_TOKEN"] || "";
-    
-    if (token === "") {
-      throw errors.noToken;
-    }
-    
-    const octokit = new github.GitHub(token);
-
-    const pulls = await octokit.pulls.list({...context.repo, state: "open"});
-
-    console.log(`pulls number: ${pulls.length}`);
-
-    for (pull in pulls) {
-      const payload = JSON.stringify(pull, undefined, 2);
-      console.log(payload);
-    }
-
-    //console.log(`comment: ${comment}!`);
-  } catch (error) {
-    core.setFailed(error.message);
+try {
+  const context = github.context;
+  const comment = core.getInput('comment');
+  const token = process.env["GITHUB_TOKEN"] || "";
+  
+  if (token === "") {
+    throw errors.noToken;
   }
+  
+  const octokit = new github.GitHub(token);
+
+  const pulls = await octokit.pulls.list({...context.repo, state: "open"});
+
+  console.log(`pulls number: ${pulls.length}`);
+
+  for (pull in pulls) {
+    const payload = JSON.stringify(pull, undefined, 2);
+    console.log(payload);
+  }
+
+  //console.log(`comment: ${comment}!`);
+} catch (error) {
+  core.setFailed(error.message);
 }
