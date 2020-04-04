7751e7b2f81bc051a1a38b607a0df1d0597ae6bc
Dario Lombardo <lomato@gmail.com>
2020-04-04 23:12:00 +0200

    Add token.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const comment = core.getInput('comment');
+  const token = process.env["GITHUB_TOKEN"] || "";
+  
+  if (token === "") {
+    throw errors.noToken;
+  }
+  
   const octokit = new github.GitHub(token);
 
   console.log(octokit.pulls.list({
