483fb8c591ad8f248785849564068080d14ba7a9
Dario Lombardo <lomato@gmail.com>
2020-04-04 23:39:51 +0200

    add async

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-  const context = github.context;
-  const comment = core.getInput('comment');
-  const token = process.env["GITHUB_TOKEN"] || "";
-  
-  if (token === "") {
-    throw errors.noToken;
+async () => {
+  try {
+    const context = github.context;
+    const comment = core.getInput('comment');
+    const token = process.env["GITHUB_TOKEN"] || "";
+    
+    if (token === "") {
+      throw errors.noToken;
+    }
+    
+    const octokit = new github.GitHub(token);
+
+    const pulls = await octokit.pulls.list({...context.repo, state: "open"});
+
+    console.log(`pulls number: ${pulls.length}`);
+
+    for (pull in pulls) {
+      const payload = JSON.stringify(pull, undefined, 2);
+      console.log(payload);
+    }
+
+    //console.log(`comment: ${comment}!`);
+  } catch (error) {
+    core.setFailed(error.message);
   }
-  
-  const octokit = new github.GitHub(token);
-
-  const pulls = await octokit.pulls.list({...context.repo, state: "open"});
-
-  console.log(`pulls number: ${pulls.length}`);
-
-  for (pull in pulls) {
-    const payload = JSON.stringify(pull, undefined, 2);
-    console.log(payload);
-  }
-
-  //console.log(`comment: ${comment}!`);
-} catch (error) {
-  core.setFailed(error.message);
 }
