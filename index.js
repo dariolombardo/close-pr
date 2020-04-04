42d90a6b5536fc31c22291d455e6d463d7c90b42
Dario Lombardo <lomato@gmail.com>
2020-04-04 23:10:42 +0200

    Log PRs.

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const comment = core.getInput('comment');
+  const octokit = new github.GitHub(token);
+
+  console.log(octokit.pulls.list({
+    ...context.repo,
+    state: "open"
+  }));
 
   console.log(`comment: ${comment}!`);
 } catch (error) {
